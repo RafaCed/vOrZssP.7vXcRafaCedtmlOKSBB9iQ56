67db069277907b903bffabd16c3eda098f7dcbeb
RafaCed <cederbergrafael@gmail.com>
2024-11-27 09:46:05 +0200

    uppgift16

--- Program.cs
+++ Program.cs
@@ -4,28 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uppgift14
+namespace uppgift16
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int num = 7;
-            int length = 5;
+            int[] myNum = {45, 12, 78, 34, 89, 23};
+            Console.WriteLine("Original list: ");
+            for (int i = 0; i < myNum.Length; i++)
+            {
+                Console.WriteLine(myNum[i]);
+            }
+            
+            Console.WriteLine("\nSorted list: ");
+            Array.Sort(myNum);
+            foreach (int i in myNum)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\nSorted list in reverse: ");
+            
+            for (int i = myNum.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(myNum[i]);
+            }
+            List<int> numList = new List<int>(myNum); 
+            numList.Remove(78); 
 
-            int[] multiplicationTable = new int[length];
-            for (int i = 0; i < length; i++)
+            myNum = numList.ToArray();
+
+            Console.WriteLine("\nArray after removing 78:");
+            foreach (int num in myNum)
             {
-                multiplicationTable[i] = (i + 1) * num;
+                Console.WriteLine(num);
             }
-            Console.WriteLine($"Multiplication table for {num}: ");
-            for (int i = 0; i < length; i++)
+            int index = Array.IndexOf(myNum, 34);
+            if (index != -1)
             {
-                Console.WriteLine($"{num} x {i + 1} = {multiplicationTable[i]}");
+                Console.WriteLine($"\nThe index of 34 is: {index}");
             }
-                
-                Console.ReadLine();
+            else
+            {
+                Console.WriteLine("\nThe number 34 doesn't exist in the array");
+            }
+
+            Console.ReadLine();
         }
-
     }
 }
