15ec2df6786c782bb2417db0a88c629504d2d74d
RafaCed <cederbergrafael@gmail.com>
2024-12-02 10:54:11 +0200

    uppgift17

--- Program.cs
+++ Program.cs
@@ -1,33 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace uppgift13
+namespace DictionaryCaseTask
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number: ");
+            Dictionary<string, string> subjects = new Dictionary<string, string>();
+            bool exitProgram = false;
+            while (!exitProgram)
+            {
+                Console.WriteLine("Menu: ");
+                Console.WriteLine("1. Add a new subject: ");
+                Console.WriteLine("2. Remove a subject: ");
+                Console.WriteLine("3. Show all: ");
+                Console.WriteLine("4. Exit. ");
+                Console.Write("Select and option (1-4): ");
 
-            int number;
-            while (!int.TryParse(Console.ReadLine(), out number));
-            {
-                Console.WriteLine("Enter a valid integrer");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddSubject(subjects);
+                        break;
+                    case "2":
+                        RemoveSubject(subjects);
+                        break;
+                    case "3":
+                        DisplaySubjects(subjects);
+                        break;
+                    case "4":
+                        exitProgram = true;
+                        Console.WriteLine("Exiting program. ");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. ");
+                        break;
+
+                }
+                Console.WriteLine();
             }
-            if (number % 2 == 0)
+        }
+        //
+        static void AddSubject(Dictionary<string, string> subjects)
+        {
+            Console.Write("Enter name of the new subject: ");
+            string subjectName = Console.ReadLine();
+            Console.Write("Enter name of the teacher: ");
+            string teacherName = Console.ReadLine();
+            
+            if (!subjects.ContainsKey(subjectName))
             {
-                Console.WriteLine($"The number {number} is even. ");
+                subjects.Add(subjectName, teacherName);
+                Console.WriteLine("Subject added successfully. ");
             }
             else
             {
-                Console.WriteLine($"The number {number} is odd. ");
+                Console.WriteLine("Subject already exists. ");
             }
-
-
-          Console.ReadLine();
+        }
+        //
+        static void RemoveSubject(Dictionary<string, string> subjects)
+        {
+            string subjectName = Console.ReadLine();
+            if (subjects.ContainsKey(subjectName))
+            {
+                subjects.Remove(subjectName);
+                Console.WriteLine("Subject removed successfully");
+            }
+            else
+            {
+                Console.WriteLine("Subject not found. ");
+            }
+        }
+        //
+        static void DisplaySubjects(Dictionary<string, string> subjects)
+        {
+            Console.WriteLine("Subjects and Teachers: ");
+            if (subjects.Count == 0)
+            {
+                Console.WriteLine("No subjects avaible. ");
+            }
+            else
+            {
+                foreach (var kvp in subjects)
+                {
+                    Console.WriteLine($"Subject: {kvp.Key}, Teacher: {kvp.Value}"); //kvp=keyValuePair
+                }
+            }
         }
     }
 }
+
+
+       
